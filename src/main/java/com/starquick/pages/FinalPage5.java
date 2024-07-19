b1b7e20f21af7d4fe958aaf8c43465345dda23e6
infinitemonitoring <infinitemonitoring09@gmail.com>
2024-07-20 00:59:28 +0530

    Update FinalPage5.java
    Co-Authored-By: Faraz Dasurkar <109261555+Dasurkarfaraz@users.noreply.github.com>

--- src/main/java/com/starquick/pages/FinalPage5.java
+++ src/main/java/com/starquick/pages/FinalPage5.java
@@ -1,17 +1,24 @@
 package com.starquick.pages;
 
 import org.openqa.selenium.By;
-
 import com.starquick.enums.waitStatergy;
 
+/**
+ * Represents the final page in the checkout process of the application.
+ * This page contains methods to interact with elements on the final checkout page.
+ */
 public final class FinalPage5 extends BasePage {
 
-	private final By proceedtocheckout = By
-			.xpath("//div[@class='checkout-page-checkout-button-container checkout-trigger']");
+    // Locator for the "Proceed to Checkout" button
+    private final By proceedtocheckout = By.xpath("//div[@class='checkout-page-checkout-button-container checkout-trigger']");
 
-	public FinalPage5 proceedtocheckout() {
-		click(waitStatergy.CLICKABLE, proceedtocheckout, "proceedtocheckout");
-		return new FinalPage5();
-	}
-
+    /**
+     * Clicks on the "Proceed to Checkout" button.
+     *
+     * @return a new instance of FinalPage5
+     */
+    public FinalPage5 proceedtocheckout() {
+        click(waitStatergy.CLICKABLE, proceedtocheckout, "proceedtocheckout");
+        return new FinalPage5();
+    }
 }
